70c7ae6bf11321b683e1ff8180a76dc2bbdf8eec
l-shirinyan <lusinshirinyan@gmail.com>
2025-08-17 20:32:22 +0400

    Implement Contact Page UI, add form validation, and integrate Google Map

---
+++ app/components/reusable/input/Textarea.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="w-full flex flex-col items-start gap-3">
+    <label
+      v-if="label"
+      :for="inputId"
+      class="text-lg text-primary font-bold"
+    >
+      {{ label }}
+    </label>
+
+    <textarea
+      :id="inputId"
+      :placeholder="placeholder"
+      :class="[
+        'w-full border border-secondary bg-white text-sm text-primary placeholder:text-secondary placeholder:text-sm rounded-xl p-4 focus:outline-0',
+        inputClass
+      ]"
+      :value="modelValue"
+      @input="onInput"
+      rows="4"
+    />
+
+    <p v-if="error" class="text-red-600 text-sm">{{ error }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  label?: string
+  placeholder?: string
+  inputId?: string
+  modelValue: string
+  inputClass?: string
+  error?: string
+}
+
+const props = defineProps<Props>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+function onInput(event: Event) {
+  const target = event.target as HTMLTextAreaElement | null
+  if (target) {
+    emit('update:modelValue', target.value)
+  }
+}
+</script>
