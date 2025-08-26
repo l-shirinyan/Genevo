1a821f647e7106a445f2cd72021917c4f7098b0d
l-shirinyan <lusinshirinyan@gmail.com>
2025-08-26 17:25:56 +0400

    Fix UI issues

--- app/components/reusable/card/PricingCard.vue
+++ app/components/reusable/card/PricingCard.vue
@@ -22,66 +22,48 @@ const hoveredCard = ref<number | string | null>(null);
 <template>
   <form>
     <div class="mx-auto w-full px-6 h-full">
-      <div
-        v-for="tier in props.tiers"
-        :key="tier.id"
-        class="group rounded-3xl w-full min-w-[260px] h-auto p-5 lg:p-8 shadow-xl bg-white hover:bg-primary transition-colors duration-300 ease-in-out hover:cursor-pointer flex flex-col justify-between"
-        @mouseenter="hoveredCard = tier.id"
-        @mouseleave="hoveredCard = null"
-      >
+      <div v-for="tier in props.tiers" :key="tier.id"
+        class="group rounded-3xl w-full min-w-[260px] h-full p-5 lg:p-8 shadow-xl bg-white hover:bg-primary transition-colors duration-300 ease-in-out hover:cursor-pointer flex flex-col justify-between"
+        :class="{ '!bg-primary': hoveredCard === tier.id }" @mouseenter="hoveredCard = tier.id"
+        @mouseleave="hoveredCard = null" @touchend="hoveredCard = null" @touchstart="hoveredCard = tier.id">
         <p class="flex items-baseline gap-x-1">
           <span
             class="md:text-6xl text-5xl font-bold tracking-tight text-primary transition-colors duration-300 ease-in-out group-hover:text-white"
-          >
+            :class="{ '!text-white': hoveredCard === tier.id }">
             {{ tier.price }}
           </span>
-          <span
-            class="text-base font-medium text-gray-purple group-hover:text-white"
-          >
+          <span class="text-base font-medium text-gray-purple group-hover:text-white"
+            :class="{ '!text-white': hoveredCard === tier.id }">
             / Leads
           </span>
         </p>
 
         <div class="flex flex-col gap-10 items-start flex-grow">
           <div class="flex flex-col items-start justify-between gap-4 mt-5">
-            <h3
-              :id="`tier-${tier.id}`"
+            <h3 :id="`tier-${tier.id}`"
               class="text-[28px] lg:text-3xl font-semibold text-primary transition-colors duration-300 ease-in-out group-hover:text-white"
-            >
+              :class="{ '!text-white': hoveredCard === tier.id }">
               {{ tier.name }}
             </h3>
-            <p class="text-base text-lavender-gray group-hover:text-white">
+            <p class="text-base text-lavender-gray group-hover:text-white"
+              :class="{ '!text-white': hoveredCard === tier.id }">
               {{ tier.description }}
             </p>
           </div>
 
-          <ul
-            role="list"
-            class="space-y-3 text-xs lg:text-base text-lavender-gray group-hover:text-white"
-          >
-            <li
-              v-for="(feature, index) in tier.features"
-              :key="`${tier.id}-${index}`"
-              class="flex gap-x-3 text-base"
-            >
-              <NuxtImg
-                :src="
-                  hoveredCard === tier.id
-                    ? '/icons/check-white.svg'
-                    : '/icons/check.svg'
-                "
-                alt="Check Icon"
-                class="w-5 h-6 flex-none transition-colors duration-300"
-              />
+          <ul role="list" class="space-y-3 text-xs lg:text-base text-lavender-gray group-hover:text-white"
+            :class="{ '!text-white': hoveredCard === tier.id }">
+            <li v-for="(feature, index) in tier.features" :key="`${tier.id}-${index}`" class="flex gap-x-3 text-base">
+              <NuxtImg :src="hoveredCard === tier.id
+                  ? '/icons/check-white.svg'
+                  : '/icons/check.svg'
+                " alt="Check Icon" class="w-5 h-6 flex-none transition-colors duration-300" />
               {{ feature }}
             </li>
           </ul>
         </div>
 
-        <Button
-          :variant="hoveredCard === tier.id ? 'white' : 'primary'"
-          class="w-full mt-6"
-        >
+        <Button :variant="hoveredCard === tier.id ? 'white' : 'primary'" class="w-full mt-6">
           {{ props.buttonText || "Choose Plan" }}
         </Button>
       </div>
