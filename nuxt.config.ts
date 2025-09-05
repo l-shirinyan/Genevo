9f1cefd59e431364a11e80252ca6eb2dfda1b9dd
Lusine <lusinshirinyan@gmail.com>
2025-09-05 20:56:00 +0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
       "f152a6df494896f4e699ed29df4163b3b5dab641115b2236822c6e03586aae82",
     cognitoBaseUrl: "https://api.cognitohq.com",
     public: {
-      apiBase: "https://orbit-stupid-periodic-stadium.trycloudflare.com",
+      apiBase: "https://xp-think-panels-therapy.trycloudflare.com",
       cognitoPublishableKey:
         "live_publishable_key_88ec4da92855501b3fed834bbbde5a79",
       cognitoBaseUrl: "https://api.cognitohq.com",
